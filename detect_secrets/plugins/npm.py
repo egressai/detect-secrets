65ba6219134a0eec1dd8f8b0abed171910d21ced
atk81 <ashutoshkushwaha81@gmail.com>
2026-06-13 15:31:15 +0530

    Detect npm env tokens

--- detect_secrets/plugins/npm.py
+++ detect_secrets/plugins/npm.py
@@ -14,4 +14,7 @@ class NpmDetector(RegexBasedDetector):
         # npmrc authToken
         # ref. https://stackoverflow.com/questions/53099434/using-auth-tokens-in-npmrc
         re.compile(r'\/\/.+\/:_authToken=\s*((npm_.+)|([A-Fa-f0-9-]{36})).*'),
+
+        # Modern npm automation/granular tokens often appear in env files and logs.
+        re.compile(r'(?<![A-Za-z0-9_])npm_[A-Za-z0-9]{20,}(?![A-Za-z0-9_])'),
     ]
